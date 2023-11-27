dc8400896b79cecd86b92bb7c3f5d1c22558ac21
bernettesvictor <victor.bernettes@student.junia.com>
2023-11-27 09:19:17 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                        ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64 # Définit la taille des tuiles
+screen_width = 1200 # Définit la largeur de l'écran
+screen_height = len(level_map) * tile_size # Calcule la hauteur de l'écran en fonction du nombre de lignes dans le niveau
